8f4b57444be30cd689adcf1c31ec3a94f08a290a
Benjamin Walkenhorst <krylon@posteo.de>
2025-06-21 17:41:23 +0200

    Add boilerplate files

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-06-21 17:41:16 krylon>
+#
+# /data/code/python/hollywoo/__init__.py
+# created on 21. 06. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+hollywoo.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
